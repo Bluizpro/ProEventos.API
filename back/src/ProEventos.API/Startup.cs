4bd324e27bc80bc9c2aa11eb163ef69d0d1ac454
Bluizpro <bluizprogramador@gmail.com>
2023-10-16 18:27:08 -0300

    criação do Front

--- back/src/ProEventos.API/Startup.cs
+++ back/src/ProEventos.API/Startup.cs
@@ -39,6 +39,7 @@ namespace ProEventos.API
             );
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -60,6 +61,7 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
