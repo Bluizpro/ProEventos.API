94e13d1348cb18f7eec0fa3284505a0828fec87e
Bluizpro <bluizprogramador@gmail.com>
2023-10-16 21:54:09 -0300

    criaçao tabelas e navBar

--- front/ProEventos-APP/src/app/eventos/eventos.component.ts
+++ front/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
 
   constructor( private http: HttpClient) {}
 
@@ -16,7 +16,7 @@ export class EventosComponent implements OnInit {
   }
   public getEventos(): void {
     this.http.get('https://localhost:7203/api/eventos').subscribe(
-      response => this.eventos = response,
+     response => this.eventos = response,
       error => console.log(error)
     );
     
